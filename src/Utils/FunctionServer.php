1d51a771f6545ef933a29d5570ee1d73838855c9
Agustin San Pedro <sanpedroagus@gmail.com>
2025-04-04 03:43:49 -0300

    comentarios v2

--- src/Utils/FunctionServer.php
+++ src/Utils/FunctionServer.php
@@ -5,7 +5,7 @@ use App\Utils\Database;
 
 class FunctionServer {
 
-    public static function jugadaServidor(PDO $pdo): ?int { //  ?int hace que puedas devolver un null o un entero.
+    public static function jugadaServidor(PDO $pdo): ?int { //  ?int hace que puedas devolver un null o un entero. Esta funcion necesita recibir la conexion (PDO) por parametro.
 
         $sql = "SELECT carta_id FROM mazo_carta WHERE mazo_id = 1 AND estado = 'en_mazo' ORDER BY RAND() LIMIT 1"; //Selecciona una carta de las disponibles, ORDER BY RAND() ordena aleatoriamente las cartas y LIMIT 1 te trae 1 sola. Esto es para traer una carta random de las disponibles.
         $stmt = $pdo->prepare($sql);
