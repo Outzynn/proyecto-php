d43c9eb9e7118d653946297e87a639b0c50bc693
Agustin San Pedro <sanpedroagus@gmail.com>
2025-06-22 03:57:07 -0300

    ajustes para evitar errores

--- src/public/index.php
+++ src/public/index.php
@@ -1,5 +1,8 @@
 <?php
 use Slim\Factory\AppFactory;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Slim\Psr7\Response;
 
 date_default_timezone_set('America/Argentina/Buenos_Aires'); // Setea la zona horaria
 
@@ -8,6 +11,26 @@ require __DIR__ . '/../../vendor/autoload.php';
 define('MAZO_SERVIDOR', 1);
 
 $app = AppFactory::create();
+
+// Middleware CORS para permitir solicitudes desde frontend (ajustar dominio)
+$app->add(function (Request $request, RequestHandler $handler) {
+    // Si es preflight OPTIONS, devolver 200 con headers CORS
+    if (strtoupper($request->getMethod()) === 'OPTIONS') {
+        $response = new Response();
+        return $response
+            ->withHeader('Access-Control-Allow-Origin', 'http://localhost:5173')
+            ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->withStatus(200);
+    }
+
+    $response = $handler->handle($request);
+
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', 'http://localhost:5173')
+        ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
 
 $app->addBodyParsingMiddleware();
 
